8c9a4712c5b96e9d300f50ff5b7dc75baa81bdf9
Róbert Varga <rob@robsplace.ca>
2018-03-07 21:35:55 -0500

    created predictable priya ai, which is more of an artifical dumbass, just picks the first available legal move

---
+++ UltimateTicTacToe.Ai/PredictablePriya.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateTicTacToe.Core;
+using UltimateTicTacToe.Core.Entities;
+using UltimateTicTacToe.Core.Interfaces;
+
+namespace UltimateTicTacToe.Ai
+{
+    public class PredictablePriya : IGameAi
+    {
+        public void MakePick(Game game, out int boardX, out int boardY, out int pickX, out int pickY)
+        {
+            boardX = 0;
+            boardY = 0;
+            pickX = 0;
+            pickY = 0;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    for (int k = 0; k < 3; k++)
+                        for (int l = 0; l < 3; l++)
+                            if (GameMaster.IsPickValid(game, i, j, k, l))
+                            {
+                                boardX = i;
+                                boardY = j;
+                                pickX = k;
+                                pickY = l;
+                                return;
+                            }
+        }
+    }
+}
