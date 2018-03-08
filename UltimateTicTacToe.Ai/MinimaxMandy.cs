68cab59b2442c369480c54a06ce348c167509026
Róbert Varga <rob@robsplace.ca>
2018-03-07 21:13:33 -0500

    extended game entity to have a ToString and Clone function created minimax mandy ai using minimax algorithm with alpha/beta pruning and my first shot at the utility function

---
+++ UltimateTicTacToe.Ai/MinimaxMandy.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateTicTacToe.Core;
+using UltimateTicTacToe.Core.Entities;
+using UltimateTicTacToe.Core.Interfaces;
+
+namespace UltimateTicTacToe.Ai
+{
+    public class MinimaxMandy : IGameAi
+    {
+        private static Random _random = new Random();
+        private const int MAXIMUM_UTILITY = 100;
+        private const int MINIMUM_UTILITY = -100;
+
+        public void MakePick(Game game, out int boardX, out int boardY, out int pickX, out int pickY)
+        {
+            boardX = boardY = pickX = pickY = 0;
+            int bestScore = Int32.MinValue;
+            List<Tuple<int, int, int, int>> bestResults = new List<Tuple<int, int, int, int>>();
+            var children = new Dictionary<Game, Tuple<int, int, int, int>>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            if (GameMaster.IsPickValid(game, i, j, k, l))
+                            {
+                                var newGame = game.Clone() as Game;
+                                GameMaster.UpdateBoard(newGame, i, j, k, l);
+                                children.Add(newGame, new Tuple<int, int, int, int>(i, j, k, l));
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var child in children.Keys)
+            {
+                var bestCaseScore = Minimax(child, game.CurrentPlayer);
+
+                if (bestCaseScore > bestScore)
+                {
+                    bestResults.Clear();
+                    bestResults.Add(children[child]);
+                    bestScore = bestCaseScore;
+                }
+                else if (bestCaseScore == bestScore)
+                {
+                    bestResults.Add(children[child]);
+                }
+            }
+
+            var winner = bestResults[_random.Next(bestResults.Count)];
+            boardX = winner.Item1;
+            boardY = winner.Item2;
+            pickX = winner.Item3;
+            pickY = winner.Item4;
+        }
+
+        private int Minimax(Game game, Players player, int depth = 3, int min = MINIMUM_UTILITY, int max = MAXIMUM_UTILITY)
+        {
+            // if leaf node or reached maximum depth, return Utility value of node
+            if (GameMaster.GetGameStatus(game) != null || depth == 0)
+            {
+                return Utility(game, player);
+            }
+
+            // derive all valid children
+            var children = new List<Game>();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            if (GameMaster.IsPickValid(game, i, j, k, l))
+                            {
+                                var newGame = game.Clone() as Game;
+                                GameMaster.UpdateBoard(newGame, i, j, k, l);
+                                children.Add(newGame);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // if it's a max node
+            if (game.CurrentPlayer == player)
+            {
+                var v = min;
+
+                foreach (var child in children)
+                {
+                    var vPrime = Minimax(child, player, depth - 1, v, max);
+                    if (vPrime > v)
+                    {
+                        v = vPrime;
+                    }
+                    if (v > max)
+                    {
+                        return max;
+                    }
+                }
+
+                return v;
+            }
+            // if it's a min node
+            else
+            {
+                var v = max;
+
+                foreach (var child in children)
+                {
+                    var vPrime = Minimax(child, player, depth - 1, min, v);
+                    if (vPrime < v)
+                    {
+                        v = vPrime;
+                    }
+                    if (v < min)
+                    {
+                        return min;
+                    }
+                }
+
+                return v;
+            }
+        }
+
+        private int Utility(Game game, Players player)
+        {
+            var gameStatus = GameMaster.GetGameStatus(game);
+
+            var result = 0;
+
+            // if this is a leaf node, return (100 - totalNumMoves) * (lostGame ? -1 : 1)
+            if (gameStatus.HasValue)
+            {
+                result = 100;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            for (int l = 0; l < 3; l++)
+                            {
+                                if (game.Board[i, j, k, l].HasValue)
+                                {
+                                    result--;
+                                }
+                            }
+                        }
+                    }
+                }
+                if ((gameStatus == GameStatuses.OWon && player == Players.X) || (gameStatus == GameStatuses.XWon && player == Players.O))
+                {
+                    result *= -1;
+                }
+            }
+            // if this isn't a leaf node, return the difference between boards won and lost
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        var boardStatus = GameMaster.GetBoardStatus(game, i, j);
+                        if (boardStatus.HasValue && boardStatus.Value != GameStatuses.Tie)
+                        {
+                            if ((boardStatus == GameStatuses.OWon && player == Players.O) || (boardStatus == GameStatuses.XWon && player == Players.X))
+                            {
+                                result++;
+                            }
+                            else
+                            {
+                                result--;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- UltimateTicTacToe.Core/Entities/Game.cs
+++ UltimateTicTacToe.Core/Entities/Game.cs
@@ -6,12 +6,68 @@ using System.Threading.Tasks;
 
 namespace UltimateTicTacToe.Core.Entities
 {
-    public class Game
+    public class Game : ICloneable
     {
         public Players CurrentPlayer { get; set; } = Players.X;
 
         public Players?[,,,] Board { get; } = new Players?[3, 3, 3, 3];
 
         public Tuple<int, int> LastPlay { get; set; } = null;
+
+        public object Clone()
+        {
+            var game = new Game()
+            {
+                CurrentPlayer = this.CurrentPlayer
+            };
+
+            if (this.LastPlay != null)
+            {
+                game.LastPlay = new Tuple<int, int>(this.LastPlay.Item1, this.LastPlay.Item2);
+            }
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    for (int k = 0; k < 3; k++)
+                        for (int l = 0; l < 3; l++)
+                            game.Board[i, j, k, l] = this.Board[i, j, k, l];
+
+            return game;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Current Player: {0}{1}", CurrentPlayer.ToString(), System.Environment.NewLine);
+
+            // for each row of games
+            for (int i = 0; i < 3; i++)
+            {
+                // for each board row in the current game row
+                for (int k = 0; k < 3; k++)
+                {
+                    // for each game column in row of games
+                    for (int j = 0; j < 3; j++)
+                    {
+                        // for each column in the board
+                        for (int l = 0; l < 3; l++)
+                        {
+                            stringBuilder.Append(Board[i, j, k, l].HasValue ? Board[i,j,k,l].Value.ToString() : "?");
+                        }
+                        if (j != 2)
+                        {
+                            stringBuilder.Append(" | ");
+                        }
+                    }
+                    stringBuilder.AppendLine();
+                }
+                if (i != 2)
+                {
+                    stringBuilder.AppendLine("---------------");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
